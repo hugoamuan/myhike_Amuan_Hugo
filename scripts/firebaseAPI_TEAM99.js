f6213d7b8474a787919db69a833924453db9f6cb
Hugo Amuan <hugo.amuan@icloud.com>
2024-02-20 10:22:38 -0800

    Finished demo 07.

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyC0-yo-ZD0-b_5Hz_-gwLCxzO2QiXK5QDY",
+    authDomain: "comp-1800-demo07-fa5cf.firebaseapp.com",
+    projectId: "comp-1800-demo07-fa5cf",
+    storageBucket: "comp-1800-demo07-fa5cf.appspot.com",
+    messagingSenderId: "760833688021",
+    appId: "1:760833688021:web:03410e5ecf07cfa49dbd5c"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
