786e240ea4b20722a2b3f88701f75653c5fc029e
Hugo Amuan <hugo.amuan@icloud.com>
2024-03-05 10:04:53 -0800

    commit1

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,17 @@
 
 
 function sayHello() {
-    //do something
+  //do something
 }
 //sayHello();    //invoke function
-
 //------------------------------------------------
 // Call this function when the "logout" button is clicked
 //-------------------------------------------------
 function logout() {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log("logging out user");
-      }).catch((error) => {
-        // An error happened.
-      });
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
 }
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -7,7 +7,6 @@ function loadSkeleton(){
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
-
 //---------------------------------------------------
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
