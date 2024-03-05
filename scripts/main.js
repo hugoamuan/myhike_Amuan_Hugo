fb4b7849d841993352c65bd623fc0405e4655b85
Hugo Amuan <hugo.amuan@icloud.com>
2024-03-05 13:17:03 -0800

    Yuhhhh

--- scripts/main.js
+++ scripts/main.js
@@ -81,7 +81,7 @@ function writeHikes() {
 }
 
 writeHikes();         //if you used method 1
-writeHikeLoop(21);    //if you used method 2
+// writeHikeLoop(21);    //if you used method 2
 
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
@@ -94,6 +94,7 @@ function displayCardsDynamically(collection) {
                 var details = doc.data().details;  // get value of the "details" key
 				var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
                 var hikeLength = doc.data().length; //gets the length field
+                var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
@@ -101,6 +102,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID;
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
@@ -109,6 +111,8 @@ function displayCardsDynamically(collection) {
 
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                
 
                 //i++;   //Optional: iterate variable to serve as unique ID
             })
